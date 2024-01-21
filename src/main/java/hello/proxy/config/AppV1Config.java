0ced574c7b09d7bfd1144a9f10619ce3e9e9d96d
Yeongsol-kim <onagu7167@gmail.com>
2024-01-22 00:24:43 +1100

    Proxy Pattern (fixed)

--- src/main/java/hello/proxy/config/AppV1Config.java
+++ src/main/java/hello/proxy/config/AppV1Config.java
@@ -9,12 +9,12 @@ public class AppV1Config {
 
     @Bean
     public OrderControllerV1 orderControllerV1() {
-        return new OrderControllerV1Impl(orderControllerV1());
+        return new OrderControllerV1Impl(orderServiceV1());
     }
 
     @Bean
     public OrderServiceV1 orderServiceV1() {
-        return new OrderServiceV1Impl(orderRepositoryV1())
+        return new OrderServiceV1Impl(orderRepositoryV1());
     }
 
     private OrderRepositoryV1 orderRepositoryV1() {
