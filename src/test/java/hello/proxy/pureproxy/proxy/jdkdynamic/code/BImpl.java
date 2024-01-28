cab98260e66308979420ec164ed6a7adf03e1a2d
Yeongsol-kim <onagu7167@gmail.com>
2024-01-29 10:13:50 +1100

    Jdk Dynamic Proxy

---
+++ src/test/java/hello/proxy/pureproxy/proxy/jdkdynamic/code/BImpl.java
@@ -0,0 +1,12 @@
+package hello.proxy.pureproxy.proxy.jdkdynamic.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BImpl implements BInterface{
+    @Override
+    public String call() {
+        log.info("B호출");
+        return "B";
+    }
+}
---
+++ src/test/java/hello/proxy/pureproxy/proxy/jdkdynamic/code/BInterface.java
@@ -0,0 +1,6 @@
+package hello.proxy.pureproxy.proxy.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+
+}
